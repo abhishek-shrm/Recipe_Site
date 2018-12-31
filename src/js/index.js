eb47c93a8ac34d382c8cfc80ccca66e56ba4dfdb
ABHISHEK SHARMA <abhishek.shrm.2195@gmail.com>
2019-01-01 02:41:27 +0530

    added axios and api

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
-// Global app controller
-import x from './test'
-const y=23;
-console.log(`${x},${y}`);
+import axios from 'axios';
+async function getResults(query){
+  const key='8c0abafa984910e38d70a9da2a67ebde';
+  try{
+  const result= await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+  const recipes=result.data.recipes;
+  console.log(recipes);
+  } catch(error){
+    alert(error);
+  }
+}
+getResults('pizza');
---
+++ src/js/views/searchView.js
@@ -0,0 +1,2 @@
+//8c0abafa984910e38d70a9da2a67ebde
+//https://www.food2fork.com/api/search
